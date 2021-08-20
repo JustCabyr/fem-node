87df65f58586a4f6d7c2a03e0d2e252352392fb3
justcabyr <asulaiman@dervac.com>
2021-08-20 18:51:11 +0100

    Added comments to Reddit CLI

--- reddit-cli/reddit.mjs
+++ reddit-cli/reddit.mjs
@@ -1,21 +1,28 @@
 #! /usr/bin/env node
+// import packages 
 import fetch from 'node-fetch'
 import open from 'open'
 import yargs from 'yargs'
 
+// parse env vars
 const { argv } = yargs(process.argv)
 
+// init fetch to reddit api
 const res = await fetch('https://www.reddit.com/.json')
 const data = await res.json()
 const children = data.data.children
+
+// get radom post from reddit api response of all posts on front page
 const randomPost = children[Math.floor(Math.random() * children.length)]
 const link = `https://www.reddit.com${randomPost.data.permalink}`
 
+// log if --print flag is passed
 if (argv.print || argv.p) {
     console.log(`
         title: ${randomPost.data.title},
         link: ${link}
     `)
 } else {
+    // open in browser if not
     open(link)
 }
